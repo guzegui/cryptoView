47d121ab5900f18454d818e2ee866af4014b90e4
guzegui <guzmanegui@gmail.com>
2024-04-18 16:02:46 +0200

    Fetch RSS feed with CORS proxy service. XML to JSON conversion pending

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -1,7 +1,23 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const url = "https://corsproxy.io/?https://cointelegraph.com/rss";
 function NewsPage() {
-  return (
-    <div>NewsPage</div>
-  )
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        const feed = response.data;
+        console.log(feed);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return <div>NewsPage</div>;
 }
 
-export default NewsPage
+export default NewsPage;
